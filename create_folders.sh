550dcdf5b78bde4fe1a3efa2531ab0548b7f5d6d
Ayomide Oyekanmi <ayomideoyekanmi@gmail.com>
2024-05-19 11:32:31 +0100

    Update create_folders.sh to create folders for groups 5-10

--- create_folders.sh
+++ create_folders.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 
-mkdir -p "weekly_assignments"
-
-for WK_NUM in {1..12}
+for GROUP in {5..10}
 do
-  formatted_week=$(printf "%02d" $WK_NUM)
-  mkdir -p "weekly_assignments/wk$formatted_week"
-  touch "weekly_assignments/wk$formatted_week/.gitkeep"
+  git clone https://github.com/wolfstation/wdsap-2024-group$GROUP.git
+  cd wdsap-2024-group$GROUP/
+  
+  mkdir -p "weekly_assignments"
+  for WK_NUM in {1..12}
+  do
+    formatted_week=$(printf "%02d" $WK_NUM)
+    mkdir -p "weekly_assignments/wk$formatted_week"
+    touch "weekly_assignments/wk$formatted_week/.gitkeep"
+  done
+  
+  mkdir -p "hackathons" && mkdir "hackathons/hack1" && mkdir "hackathons/hack2"
+  touch "hackathons/hack1/.gitkeep" && touch "hackathons/hack2/.gitkeep"
+  
+  mkdir -p "project"
+  touch "project/.gitkeep"
+  
+  git add .
+  git commit -am "Setup assessment folders"
+  git push
+
+  cd ..
 done
-
-mkdir -p "hackathons" && mkdir "hackathons/hack1" && mkdir "hackathons/hack2"
-touch "hackathons/hack1/.gitkeep" && touch "hackathons/hack2/.gitkeep"
-
-mkdir -p "project"
-touch "project/.gitkeep"
-
-git add .
-git commit -am "Setup assessment folders"
-git push
