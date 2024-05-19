dc349eca2facb8fb774eb02cfb1ba7bf4167d2ce
Ayomide Oyekanmi <ayomideoyekanmi@gmail.com>
2024-05-19 11:21:36 +0100

    Add .gitkeep to all folders
    Git doesn't track empty folders

--- create_folders.sh
+++ create_folders.sh
@@ -6,8 +6,11 @@ for WK_NUM in {1..12}
 do
   formatted_week=$(printf "%02d" $WK_NUM)
   mkdir -p "weekly_assignments/wk$formatted_week"
+  touch "weekly_assignments/wk$formatted_week/.gitkeep"
 done
 
 mkdir -p "hackathons" && mkdir "hackathons/hack1" && mkdir "hackathons/hack2"
+touch "hackathons/hack1/.gitkeep" && touch "hackathons/hack2/.gitkeep"
 
 mkdir -p "project"
+touch "project/.gitkeep"
